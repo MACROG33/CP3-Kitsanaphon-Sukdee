ec26d45bf801e42d9025ca5d8e09406cb9efdc1b
MACROG33 <Kitsanaphon.sukdee@gmail.com>
2022-07-31 08:46:26 +0700

    Create Exercise4_Kitsanaphon_S.py

---
+++ Exercise4_Kitsanaphon_S.py
@@ -0,0 +1,9 @@
+FE = 90.5
+GB = 73.2
+ITCS = 83.5
+CP = 90.5
+print("---Your Score---")
+print("Foundation English :",FE)
+print("General Business : ",GB)
+print("Introduction to CS :",ITCS)
+print("Computer Programming :",CP)
