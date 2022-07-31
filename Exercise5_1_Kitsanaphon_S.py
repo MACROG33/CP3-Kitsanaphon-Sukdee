312f2afa72f07375a45956f271c9d9b8e199a432
MACROG33 <Kitsanaphon.sukdee@gmail.com>
2022-07-31 11:46:04 +0700

    Create Exercise5_1_Kitsanaphon_S.py

---
+++ Exercise5_1_Kitsanaphon_S.py
@@ -0,0 +1,6 @@
+Num1 = int(input("Number1: "))
+Num2 = int(input("Number2: "))
+print(Num1,"+",Num2,"=",Num1+Num2)
+print(Num1,"-",Num2,"=",Num1-Num2)
+print(Num1,"*",Num2,"=",Num1*Num2)
+print(Num1,"/",Num2,"=",Num1/Num2)
