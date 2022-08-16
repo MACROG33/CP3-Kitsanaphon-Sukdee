0b99770a87b520cec172585aaa9090b200568d15
MACROG33 <Kitsanaphon.sukdee@gmail.com>
2022-08-16 19:56:15 +0700

    Create Lecture50.py

---
+++ Lecture50.py
@@ -0,0 +1,15 @@
+def addNumber(x,y):
+    print(x+y)
+addNumber(20,15)
+
+def deleteNumber(x,y):
+    print(x-y)
+deleteNumber(20,15)
+
+def multiplyNumbers(x,y):
+    print(x*y)
+multiplyNumbers(20,15)
+
+def dividetheNumber(x,y):
+    print(x/y)
+dividetheNumber(20,15)
