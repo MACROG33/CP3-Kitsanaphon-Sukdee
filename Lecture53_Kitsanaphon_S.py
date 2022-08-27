dab7e844460417340dfd38d341c7bf7f6e70e2bd
MACROG33 <Kitsanaphon.sukdee@gmail.com>
2022-08-27 11:02:55 +0700

    Create Lecture53_Kitsanaphon_S.py

---
+++ Lecture53_Kitsanaphon_S.py
@@ -0,0 +1,6 @@
+price = input = int(input("Price: "))
+def vatCalculate(totalPrice):
+    result = totalPrice+(totalPrice*7/100)
+    return result
+print(vatCalculate(price))
+print("-------successfully-------")
